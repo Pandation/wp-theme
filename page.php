1a46bd3da09286fca325bb4aeadc9ccf03872bc0
Pandation <f.baumes@laposte.net>
2021-08-23 15:38:13 +0200

    learn how to get the page parent id

--- page.php
+++ page.php
@@ -8,7 +8,7 @@ while (have_posts()) {
     <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri("/images/ocean.jpg"); ?>)"></div>
       <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title() ?></h1>
+        <h1 class="page-banner__title"><?php the_title();?></h1>
         <div class="page-banner__intro">
           <p>Don't FORGET TO REPLACE ME</p>
         </div>
@@ -16,11 +16,20 @@ while (have_posts()) {
     </div>
 
     <div class="container container--narrow page-section">
+
+<?php
+
+$theParent = wp_get_post_parent_id(get_the_ID());
+if ($theParent) {?>
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
+          <a class="metabox__blog-home-link" href="<?php echo get_the_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title();?></span>
         </p>
       </div>
+ <?php }
+    echo "Coucou";
+    ?>
+
       <!--
       <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
@@ -31,7 +40,7 @@ while (have_posts()) {
       </div>
 -->
       <div class="generic-content">
-       <?php the_content() ?>
+       <?php the_content()?>
       </div>
     </div>
 
